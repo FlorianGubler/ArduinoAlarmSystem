fb8d3c86c02595ed119a353bf6dece6d535ff7fb
Florian Gubler <gubler.florian@gmx.net>
2023-05-11 08:12:59 +0200

    Commenting

--- AlarmAPI/src/main/java/io/github/floriangubler/alarmapi/configuration/WebSocketConfig.java
+++ AlarmAPI/src/main/java/io/github/floriangubler/alarmapi/configuration/WebSocketConfig.java
@@ -6,18 +6,21 @@ import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBr
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+/* WebSocket Configuration for Alarm Socket */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
+        //Create alarm-message channel
         config.enableSimpleBroker("/alarm-message");
         config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
+        //Register Socket endpoint and enable for socketJS
         registry.addEndpoint("/alarm-socket");
         registry.addEndpoint("/alarm-socket").withSockJS();
     }
